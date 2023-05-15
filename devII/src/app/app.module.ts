50fa6d80af44663b3a489cd93a4effbc31d932de
Adrio <adriorosa267@gmail.com>
2023-05-15 16:20:47 -0300

    modified:   devII/package-lock.json 	modified:   devII/package.json 	modified:   devII/src/app/app.component.html 	modified:   devII/src/app/app.module.ts 	new file:   devII/src/app/formulario-servidor/formulario-servidor.component.html 	new file:   devII/src/app/formulario-servidor/formulario-servidor.component.scss 	new file:   devII/src/app/formulario-servidor/formulario-servidor.component.spec.ts 	new file:   devII/src/app/formulario-servidor/formulario-servidor.component.ts

--- devII/src/app/app.module.ts
+++ devII/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormularioServidorComponent } from './formulario-servidor/formulario-servidor.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, FormularioServidorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ devII/src/app/formulario-servidor/formulario-servidor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-servidor',
+  templateUrl: './formulario-servidor.component.html',
+  styleUrls: ['./formulario-servidor.component.scss']
+})
+export class FormularioServidorComponent {
+
+}
