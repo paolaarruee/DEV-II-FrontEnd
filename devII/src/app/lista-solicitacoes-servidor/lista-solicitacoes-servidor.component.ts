3f9c2849ed33a71d4cd906c429f0e90cc01294f0
vinivcs97 <vinivcs97@gmail.com>
2023-06-13 02:08:32 -0300

    modified:   devII/src/app/lista-solicitacoes-servidor/dados-backend.service.ts 	modified:   devII/src/app/lista-solicitacoes-servidor/lista-solicitacoes-servidor.component.ts

--- devII/src/app/lista-solicitacoes-servidor/lista-solicitacoes-servidor.component.ts
+++ devII/src/app/lista-solicitacoes-servidor/lista-solicitacoes-servidor.component.ts
@@ -9,6 +9,7 @@ import { DadosBackendService } from './dados-backend.service';
 })
 export class ListaSolicitacoesServidorComponent {
 
+  /* //solicitações para receber do backend 
    
     listaSolicitacoes: any[] = [];
   
@@ -36,7 +37,50 @@ export class ListaSolicitacoesServidorComponent {
         }
       );
     }
+  */
+
+    //lista de Solicitações para teste
+    listaSolicitacoes: any[] = [];
+
+    constructor() {}
   
-  
+    ngOnInit() {
+      // Simulação de dados de solicitações
+      const solicitacao1 = {
+        alunoId: 1,
+        servidorId: 1,
+        tipo: 'estagio',
+        status: 'aprovado',
+        titulo: 'Solicitação 1',
+        conteudo: 'Conteúdo da solicitação 1',
+        observacao: 'Observação da solicitação 1',
+        etapa: 1
+      };
+      const solicitacao2 = {
+        alunoId: 2,
+        servidorId: 1,
+        tipo: 'estagio',
+        status: 'em andamento',
+        titulo: 'Solicitação 2',
+        conteudo: 'Conteúdo da solicitação 2',
+        observacao: 'Observação da solicitação 2',
+        etapa: 2
+      };
+      const solicitacao3 = {
+        alunoId: 3,
+        servidorId: 2,
+        tipo: 'estagio',
+        status: 'negado',
+        titulo: 'Solicitação 3',
+        conteudo: 'Conteúdo da solicitação 3',
+        observacao: 'Observação da solicitação 3',
+        etapa: 3
+      };
+    
+      // Adiciona as solicitações ao array
+      this.listaSolicitacoes.push(solicitacao1);
+      this.listaSolicitacoes.push(solicitacao2);
+      this.listaSolicitacoes.push(solicitacao3);
+    }
 
 }
